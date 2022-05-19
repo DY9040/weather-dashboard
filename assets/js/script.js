a05e69169eaa2c050e38f092073d096cc059d6bd
Forever Young <dy9040@gmail.com>
2022-05-18 19:42:55 -0500

    added api key and bootstrap

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//api call https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+const APIKey = "bebfddd4f8eb58703065f33ecf495871"
+
+//api Coordinates by city name http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
