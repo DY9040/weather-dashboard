94465cdbf8c2823333fdf7b7ee2c64095bfc4961
Forever Young <dy9040@gmail.com>
2022-05-30 23:33:54 -0500

    added styling and new README

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# weather-dashboard
+# Weather Dashboard ![bad math](https://img.shields.io/badge/License-MIT-blue)
+
+## Description
+This is a weather dashboard that will give you the forecast of whatever city you're requesting.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Credits](#credits)
+- [Contributions](#contributions)
+- [Testing](#testing)
+- [Questions](#questions)
+
+## Installation
+You can clone the repo from my github profile or visit the deployed link.
+
+## Usage
+![screenshot](/assets/images/Capture.PNG "screenshot")
+Type into the search bar the city you want weather info on and click search. the Dashboard will populate the current weather as well as the 5 day forecast.
+
+## License
+
+![bad math](https://img.shields.io/badge/License-MIT-blue)
+This project is using MIT. For more information on this or other licenses please reference: [https://choosealicense.com](https;//choosealicense.com).
+
+MIT
+
+## Credits
+N/A
+
+## Contributions
+This project is using [Contributor Covenant] (https://www.contributor-covenant.org/) Please read over their guidelines before contributing.
+
+## Testing Instructions
+N/A
+
+## Questions
+You can contact me via my [Github Profile](https://github.com/dy9040)
+or by email test@gmail.com if you have any questions.
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,6 @@
 
 var API_Key ='cba0d561fef3195ac96f2d675321d911'
 
-//api Coordinates by city name https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-// EX. https://api.openweathermap.org/data/2.5/onecall?
-// on screen///
-
-// var cityName = "Richmond"
-// var Coordinates =("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=cba0d561fef3195ac96f2d675321d911");
-
-// fetch(Coordinates).then((response) => {
-// response.json().then((data) => {
-// console.log("https://api.openweathermap.org/data/2.5/oncall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&appid=cba0d561fef3195ac96f2d675321d911")
-// });
-
-// });
 
 
 
@@ -35,17 +22,17 @@ var recentSearch1= document.querySelector("#recent-search-1");
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
-    var city = cityInputEl.value.trim()
+    var city = cityInputEl.value.trim() || NewCity;
 
     if(city) {
         getCityCoords(city);
         cityInputEl.value ="";
         weatherInfoEl.textContent ="";
-        // day1EL.textContent ="";
-        // day2El.textContent ="";
-        // day3El.textContent ="";
-        // day4El.textContent ="";
-        // day5El.textContent ="";
+        day1El.textContent ="";
+        day2El.textContent ="";
+        day3El.textContent ="";
+        day4El.textContent ="";
+        day5El.textContent ="";
         grabCityName(city);
     } else {
         alert("Please enter a city");
@@ -56,11 +43,11 @@ citySearchFormEl.addEventListener('submit', formSubmitHandler);
 
 // city -> lat and lon coordinates
 var getCityCoords = function(city) {
-    var ApiCoordinates = ("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=cba0d561fef3195ac96f2d675321d911");
+    var ApiCoordinates = ("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + API_Key);
         fetch(ApiCoordinates).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                var pickedCity = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&units=imperial&appid=cba0d561fef3195ac96f2d675321d911");
+                var pickedCity = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&units=imperial&appid=" + API_Key);
                 displayWeather(pickedCity);
                 displayForecast(pickedCity);
                 displayForecast2(pickedCity);
@@ -82,30 +69,39 @@ var displayWeather = function(pickedCity) {
         response.json().then(function(data) {
 
             //shows Icons 
-            console.log (data.current.weather[0].icon);
             var extantIconEl = document.createElement("img")
             extantIconEl.src = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png"
-            weatherInfoEl.appendChild(extantIconEl)
+            weatherInfoEl.append(extantIconEl)
 
             //Shows Current Temp
             var extantTempEl = document.createElement("p")
             extantTempEl.textContent = "Temp: " + data.current.temp + "\xB0F"
-            weatherInfoEl.appendChild(extantTempEl);
+            weatherInfoEl.append(extantTempEl);
 
             //Shows Current Wind Speed
             var extantWindSpeedEl = document.createElement("p")
             extantWindSpeedEl.textContent = "Wind Speed: " + data.current.wind_speed + " MPH"
-            weatherInfoEl.appendChild(extantWindSpeedEl);
+            weatherInfoEl.append(extantWindSpeedEl);
 
             //Shows Current Humidity
             var extantHumidityEl = document.createElement("p")
             extantHumidityEl.textContent = "Humidity: " + data.current.humidity + "%"
-            weatherInfoEl.appendChild(extantHumidityEl);
+            weatherInfoEl.append(extantHumidityEl);
 
-            //Shows Current UV Index
+            //Shows Current UV Index with a color
+            var UVIndexDiv = document.createElement("div");
+            weatherInfoEl.append(UVIndexDiv)
+            UVIndexDiv.classList.add("uv-index-div")
+
+            var UVIndex = document.createElement("p")
+            UVIndex.textContent = "UV Index: " 
+
             var extantUVIndexEl = document.createElement("p")
-            extantUVIndexEl.textContent = "UV Index: " + data.current.uvi
-            weatherInfoEl.appendChild(extantUVIndexEl);
+            extantUVIndexEl.textContent = data.current.uvi
+            if (data.current.uvi < 2) {extantUVIndexEl.classList.add("uv-index-green")
+            } else if (data.current.uvi < 5) {extantUVIndexEl.classList.add("uv-index-yellow")
+            } else {extantUVIndexEl.classList.add("uv-index-red")}
+            UVIndexDiv.append(UVIndex, extantUVIndexEl);
         });
     });
 
@@ -113,15 +109,14 @@ var displayWeather = function(pickedCity) {
 
 // // get city name for the openweathermap api and renders it on the screen
 var grabCityName = function(city) {
-    var cityName = ("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=cba0d561fef3195ac96f2d675321d911");
+    var cityName = ("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + API_Key);
 
     fetch(cityName).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
 
             var currentWeatherEl = document.createElement("h4");
-            currentWeatherEl.textContent =data[0].name + "(" + moment().format('1') + ")"
-//             weatherInfoEl.classList.add('weather-info');
+            currentWeatherEl.textContent =data[0].name + " (" + moment().format('l') + ")"
+            weatherInfoEl.classList.add('weather-info');
             weatherInfoEl.append(currentWeatherEl);
         });
     })
@@ -136,13 +131,13 @@ var displayForecast = function(pickedCity) {
 
             // Shows Date
             var extantDateEl = document.createElement("h5")
-            extantDateEl.textContent = moment().add(1, 'days').format('1')
+            extantDateEl.textContent = moment().add(1, 'days').format('l')
             day1El.append(extantDateEl);
 
        //shows Icons 
-        console.log (data.daily[0].weather[0].icon);
+        // console.log (data.daily[0].weather[0].icon);
         var extantIconEl = document.createElement("img")
-        extantIconEl.src = "http://openweathermap.org/img/wn/" + data.daily.weather[0].icon + ".png"
+        extantIconEl.src = "http://openweathermap.org/img/wn/" + data.daily[0].weather[0].icon + ".png"
         day1El.append(extantIconEl)
 
        //Shows Current Temp
@@ -179,11 +174,11 @@ var displayForecast2 = function(pickedCity) {
 
              // Shows Date
                 var extantDateEl = document.createElement("h5")
-                extantDateEl.textContent = moment().add(2, 'days').format('1')
+                extantDateEl.textContent = moment().add(2, 'days').format('l')
                 day2El.append(extantDateEl);
 
        //shows Icons 
-        console.log (data.current.weather[0].icon);
+        // console.log (data.current.weather[0].icon);
         var extantIconEl = document.createElement("img")
         extantIconEl.src = "http://openweathermap.org/img/wn/" + data.daily[1].weather[0].icon + ".png"
         day2El.append(extantIconEl)
@@ -221,10 +216,10 @@ var displayForecast3 = function(pickedCity) {
 
             // Shows Date
             var extantDateEl = document.createElement("h5")
-            extantDateEl.textContent = moment().add(3, 'days').format('1')
+            extantDateEl.textContent = moment().add(3, 'days').format('l')
             day3El.append(extantDateEl);
        //shows Icons 
-        console.log (data.current.weather[0].icon);
+        // console.log (data.current.weather[0].icon);
         var extantIconEl = document.createElement("img")
         extantIconEl.src = "http://openweathermap.org/img/wn/" + data.daily[2].weather[0].icon + ".png"
         day3El.append(extantIconEl)
@@ -262,11 +257,11 @@ var displayForecast4 = function(pickedCity) {
 
             // Shows Date
             var extantDateEl = document.createElement("h5")
-            extantDateEl.textContent = moment().add(4, 'days').format('1')
+            extantDateEl.textContent = moment().add(4, 'days').format('l')
             day4El.append(extantDateEl);
 
        //shows Icons 
-        console.log (data.current.weather[0].icon);
+        // console.log (data.current.weather[0].icon);
         var extantIconEl = document.createElement("img")
         extantIconEl.src = "http://openweathermap.org/img/wn/" + data.daily[3].weather[0].icon + ".png"
         day4El.append(extantIconEl)
@@ -304,10 +299,10 @@ var displayForecast5 = function(pickedCity) {
 
             // Shows Date
             var extantDateEl = document.createElement("h5")
-            extantDateEl.textContent = moment().add(5, 'days').format('1')
+            extantDateEl.textContent = moment().add(5, 'days').format('l')
             day5El.append(extantDateEl);
        //shows Icons 
-        console.log (data.current.weather[0].icon);
+        // console.log (data.current.weather[0].icon);
         var extantIconEl = document.createElement("img")
         extantIconEl.src = "http://openweathermap.org/img/wn/" + data.daily[4].weather[0].icon + ".png"
         day5El.append(extantIconEl)
@@ -337,3 +332,38 @@ var displayForecast5 = function(pickedCity) {
 
     });
 };
+
+var recentSearch1El = document.querySelector('#recent-search-1');
+// got from stackoverflow.com/
+
+var storedSearches = function (event) {
+
+    event.preventDefault();
+var entries = JSON.parse(localStorage.getItem('allCities'));
+    if (entries === null) entries = [];
+    var cityInput = cityInputEl.value.trim();
+    var city = {cityList: cityInput};
+
+    localStorage.setItem('allCities', JSON.stringify(city));
+    entries.push(city);
+    localStorage.setItem('allCities', JSON.stringify(entries));
+    if (cityInput) {
+        for (var i = 0; i < entries.length; i++) {
+            var cityOne = JSON.parse(localStorage.getItem('fiveCities'))
+
+            var searchOne = document.createElement('button');
+            searchOne.textContent = cityOne.slice(-1)[0].cityList;
+            recentSearch1El.appendChild(searchOne);
+        };
+    }
+    searchOne.addEventListener('click', function(e) {
+        var NewCity = e.target.innerText
+        formSubmitHandler(NewCity);
+    });
+};
+
+
+citySearchFormEl.addEventListener('submit', function() {
+    storedSearches(event);
+    formSubmitHandler(event);
+});
